a268020cf9447df8a398e996a2897e8da8b283e3
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-11-29 08:53:14 +0530

    hello

---
+++ 29-11-24/india.js
@@ -0,0 +1,27 @@
+let { log } = console;
+
+//! 1] default Export :
+
+//! In default export we have to use default keyword.
+//! We can write default keyword only once.
+//! If we want to export multiple variables in default way, We can wrap the variables into an object & then we will export that object.
+//! When it is default export, while importing we can use any name.
+//! When it is default export, while importing we sould not take curly braces ( {} )
+//! When we are exporting as default, default keyword should not be on the same line of variable declaration.
+
+// import planes from "./russia.js";
+
+// log(planes);
+
+//! 2] Named Export :
+
+//! In Named export we shoud not use default keyword.
+//! We can do named export mmultiple times.
+//! When it is named export, while importing we shold take same exported name.
+//! When it is named export, while importing we must take curly braces ( {} )
+//! When we are exporting as named export, export keyword can be on the same line of variable declaration.
+
+import { plane1, plane2 } from "./russia.js";
+
+log(plane1);
+log(plane2);
---
+++ 29-11-24/russia.js
@@ -0,0 +1,14 @@
+//! 1] default Export :
+
+// let plane1 = "Fighter Jet";
+// let plane2 = "Cargo Plane";
+
+// export default {
+//   plane1,
+//   plane2,
+// };
+
+//! 2] Named Export :
+
+export let plane1 = "Fighter Plane";
+export let plane2 = "Cargo Plane";
